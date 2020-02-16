238680dec00a2c896f8685481463958a0439e983
lenochware <lenochware@gmail.com>
2020-02-16 15:54:01 +0100

    rewritten

--- ship.js
+++ ship.js
@@ -6,11 +6,16 @@ class Main extends NextGame {
 		this.canvas.width(800);
 		this.canvas.height(600);
 
-		this.ship = new Shape(300, 300, [10, 0, -10, 10, -10, -10]);
-		this.particles = new Particles(this.canvas);
-		this.enemies = new Particles(this.canvas);
+		this.ship = new Ship(this.canvas, 300, 300, [10, 0, -10, 10, -10, -10]);
 
-		this.enemies.add(new Enemy(50,50, 1, 1, 'white'));
+		this.spaceObjects = new Group();
+		this.particles = new Group();
+
+		this.spaceObjects.add(this.ship);
+		
+		let mob = new Enemy(this.canvas, 100, 100);
+		mob.setVelocity(1,3);
+		this.spaceObjects.add(mob);
 	}
 
 	update()
@@ -36,13 +41,15 @@ class Main extends NextGame {
 		}
 
 		this.ship.angle = Math.atan2(this.mouse.y - this.ship.y, this.mouse.x - this.ship.x);
-		this.ship.update();
-		this.ship.draw(this.canvas);
+
+		this.spaceObjects.update();
 		this.particles.update();
-		this.enemies.update();
 
 		if(this.mouse.buttons) {
-			this.particles.add(new Particle(this.ship.x, this.ship.y, 5, this.ship.angle, 'red'));
+			let p = new Particle(this.canvas, this.ship.x, this.ship.y);
+			p.setVelocity(5, this.ship.angle);
+			p.color = 'red';
+			this.particles.add(p);
 			this.mouse.buttons = 0;
 		}
 
@@ -52,98 +59,70 @@ class Main extends NextGame {
 
 class Particle
 {
-	constructor(x, y, v, angle, color)
+	constructor(canvas, x, y)
 	{
-		this.color = color;
-		this.x = x;
-		this.y = y;
-		this.dx = Math.cos(angle);
-		this.dy = Math.sin(angle);
-		this.decel = 0;
-		this.v = v;
-		this.dead = false;
-	}
-
-	update()
-	{
-		if (this.dead) return;
-		this.x += this.dx * this.v;
-		this.y += this.dy * this.v;
-
-	}
-
-	draw(canvas)
-	{
-		if (this.dead) return;
-		canvas.rectf(this.x, this.y, 5, 5, this.color);
-
-		if (this.x < 0 || this.y < 0 || this.x > canvas.width() || this.y > canvas.height()) {
-			this.dead = true;
-		}
-	}
-}
-
-class Enemy extends Particle
-{
-	draw(canvas)
-	{
-		if (this.dead) return;
-		canvas.rect(this.x, this.y, 40, 40, this.color);
-
-		if (this.x < 0 ||  this.x > canvas.width()) this.dx *= -1;
-		if (this.y < 0 || this.y > canvas.height()) this.dy *= -1;
-	}
-
-}
-
-class Particles
-{
-	constructor(canvas)
-	{
-		this.data = [];
 		this.canvas = canvas;
-		this.deadCount = 0;
-	}
-
-	update()
-	{
-		for(let p of this.data) {
-			if (p.dead) continue;
-			p.update();
-			p.draw(this.canvas);
-			if (p.dead) this.deadCount++;
-		}
-
-		if (this.data.length > 10 && this.deadCount > 5) {
-			this.cleanUp();
-		}
-	}
-
-	cleanUp()
-	{
-		this.data = this.data.filter(p => p.dead == false);
-		console.log('cleanUp', this.data.length);
-	}
-
-	add(p)
-	{
-		this.data.push(p);
-	}
-}
-
-class Shape
-{
-	constructor(x, y, nodes)
-	{
-		this.nodes = nodes;
-		this.color = 'white';
 		this.x = x;
 		this.y = y;
 		this.vx = 0;
 		this.vy = 0;
-		this.decel = 0.1;
+		this.ax = 0;
+		this.ay = 0;
+		this.color = 'white';
+		this.size = 5;
+		this.alive = true;
+	}
 
-		this.angle = 1;
+	setVelocity(size, angle)
+	{
+		this.vx = size * Math.cos(angle);
+		this.vy = size * Math.sin(angle);
+	}
+
+	update()
+	{
+		if (this.outOfScreen()) this.alive = false;
+		if (!this.alive) return;
+
+		this.vx += this.ax;
+		this.vy += this.ay;
+		this.x += this.vx;
+		this.y += this.vy;
+
+		this.draw();
+	}
+
+	collides(mob)
+	{
+		if (this == mob) return false;
+		return ((mob.x - this.x) ** 2 + (mob.y - this.y) ** 2 <= (mob.size + this.size) **2);
+	}
+
+	outOfScreen()
+	{
+		if (this.x < 0 ||  this.x > this.canvas.width()) return 1;
+		if (this.y < 0 || this.y > this.canvas.height()) return 2;
+		return 0;
+	}
+
+	hit(source)
+	{
+		this.alive = false;
+	}
+
+	draw()
+	{
+		this.canvas.rectf(this.x, this.y, this.size, this.size, this.color);
+	}
+}
+
+class Mob extends Particle
+{
+
+	constructor(canvas, x, y, nodes)
+	{
+		super(canvas,x, y);
+		this.nodes = nodes;
 	}
 
 	translate(nodes, x, y)
@@ -172,26 +151,104 @@ class Shape
 		return output;
 	}
 
+	draw()
+	{
+		this.x = Utils.clamp(this.x, 0, this.canvas.width());
+		this.y = Utils.clamp(this.y, 0, this.canvas.height());
+
+		let nodes = this.translate(this.rotate(this.nodes, this.angle), this.x, this.y);
+
+		this.canvas.polygon(nodes, this.color);
+	}	
+}
+
+class Ship extends Mob
+{
+	constructor(canvas, x, y, nodes)
+	{
+		super(canvas, x, y, nodes);
+		this.decel = 0.1;
+		this.angle = 1;
+	}
 
 	update()
 	{
 		this.vx += -Math.sign(this.vx) * this.decel;
 		this.vy += -Math.sign(this.vy) * this.decel;
 
-		if (Math.abs(this.vx) < 0.1) this.vx = 0;
-		if (Math.abs(this.vy) < 0.1) this.vy = 0;
+		let off = this.outOfScreen();
+		if (off == 1) this.vx *= -1;
+		if (off == 2) this.vy *= -1;
 
 		this.x += this.vx;
 		this.y += this.vy;
+
+		this.draw();
+	}
+}
+
+class Enemy extends Particle
+{
+	update()
+	{
+		let off = this.outOfScreen();
+		if (off == 1) this.vx *= -1;
+		if (off == 2) this.vy *= -1;
+
+		this.x += this.vx;
+		this.y += this.vy;
+
+		this.draw();
 	}
 
-	draw(canvas)
+	draw()
 	{
-		this.x = Utils.clamp(this.x, 0, canvas.width());
-		this.y = Utils.clamp(this.y, 0, canvas.height());
+		this.canvas.rect(this.x, this.y, 40, 40, this.color);
+	}
 
-		let nodes = this.translate(this.rotate(this.nodes, this.angle), this.x, this.y);
+}
 
-		canvas.polygon(nodes, this.color);
+class Group
+{
+	constructor()
+	{
+		this.members = [];
+		this.deadCount = 0;
+	}
+
+	update()
+	{
+		for(let m of this.members) {
+			if (!m.alive) continue;
+			m.update();
+			if (!m.alive) this.deadCount++;
+		}
+
+		if (this.members.length > 10 && this.deadCount > 5) {
+			this.cleanUp();
+		}
+	}
+
+	collides(group)
+	{
+		for(let m of this.members) {
+			for(let n of group.members) {
+				if (m.collides(n)) {
+					m.hit(n);
+					n.hit(m);
+				}
+			}
+		}
+	}
+
+	cleanUp()
+	{
+		this.members = this.members.filter(p => p.alive);
+		console.log('cleanUp', this.members.length);
+	}
+
+	add(m)
+	{
+		this.members.push(m);
 	}
 }
