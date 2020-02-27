5d523461db4110cce37b83b530abf86ca10467e6
lenochware <lenochware@gmail.com>
2020-02-27 14:37:19 +0100

    +scale, drag

--- tiles.js
+++ tiles.js
@@ -5,6 +5,9 @@ class Main extends NextGame {
 		super.init();
 		this.canvas.width(800);
 		this.canvas.height(600);
+
+		this.offsetX = 0;
+		this.offsetY = 0;		
 	}
 
 	preload()
@@ -16,13 +19,42 @@ class Main extends NextGame {
 	{
 		this.requestUpdate();
 
+		this.canvas.resetTransform();
+		this.canvas.clear();
+		this.canvas.applyTransform();
+
 		let tileSet = new Tileset(this, this.assets.tiles, 16, 4);
 
-		this.canvas.clear();
-		//this.canvas.context.drawImage(this.assets.tiles.data, 10,10);
-		
-		tileSet.draw(13+16, 10,10);
+		for (let y = 0; y < 20; y++) {
+			for(let x = 0; x < 40; x++) {
+				Utils.seed = y * 40 + x;
+				tileSet.draw(Utils.seedRandom(1, tileSet.size - 1), x * tileSet.tileWidth, y * tileSet.tileHeight);
+			}
+		}
 
+		if (this.kb.key == 'Escape') {
+			this.offsetX = this.offsetY = 0;
+			this.canvas.offsetX = this.canvas.offsetY = 0;
+			this.canvas.scale = 1;
+			this.kb.key = '';
+		};
+
+		if (this.mouse.deltaY) {
+			this.canvas.scale *= (this.mouse.deltaY > 0? 0.95 : 1.05 );
+			this.mouse.deltaY = 0;
+		}		
+
+		if (this.mouse.hold == this.MB_MIDDLE) {
+			this.canvas.offsetX = this.offsetX + this.mouse.offsetX;
+			this.canvas.offsetY = this.offsetY + this.mouse.offsetY;
+		}
+
+		if (this.mouse.release) {
+			this.offsetX = this.canvas.offsetX;
+			this.offsetY = this.canvas.offsetY;
+			this.mouse.release = 0;
+		}		
+		
 		this.time = this.now();
 	}
 }
@@ -33,6 +65,7 @@ class Tileset
 	{
 		this.canvas = game.canvas;
 		this.image = asset.data;
+		this.size = x * y;
 		this.x = x;
 		this.y = y;
 		this.tileWidth = Math.floor(this.image.width / x);
