a45aa7a95fb2af853290c5c667e441c0ded1fb4c
lenochware <lenochware@gmail.com>
2020-02-29 15:46:04 +0100

    sum y

--- perlin.js
+++ perlin.js
@@ -19,14 +19,17 @@ class Main extends NextGame {
 		this.requestUpdate();
 		this.canvas.clear();
 
-		let base = 200;
+		let base = 300;
 		let width = this.canvas.width();
 		Utils.seed = this.seed;
 
+		let y = 200;
+
 		for (let x = 0; x < width; x++)
 		{
-			let y = Utils.seedRandFloat() * base;
-			this.canvas.line(x, y, x, base, 'lime');
+			//let y = Utils.seedRandFloat() * base;
+			y += Utils.seedRandom(-3,4);
+			this.canvas.line(x, y, x, base, y>base? 'blue':'lime');
 		}
 
 		if (this.kb.key == 'n') {
