f1cc40e42f25850031235ffe1490cec644e9ffc4
lenochware <lenochware@gmail.com>
2020-03-05 15:10:25 +0100

    warp!

--- warp.js
+++ warp.js
@@ -22,6 +22,7 @@ class Main extends NextGame {
 		this.requestUpdate();
 
 		//this.warp.scale = 1.5;
+		this.warp.time += 0.01;
 		this.warp.draw(this.screen, 100, 100, 150, 150);
 		this.canvas.image(this.screen);
 
@@ -44,6 +45,11 @@ class WarpTexture
 		this.game = game;
 		this.image = game.canvas.getImageData(game.assets[asset].data);
 		this.scale = 1;
+		this.time = 0;
+	}
+
+	warp(w) {
+		return 10 * Math.sin(this.time + w/30);
 	}
 
 	draw(im, x0, y0, w, h)
@@ -51,8 +57,11 @@ class WarpTexture
 		for(let y = 0; y < h; y++) {
 			for(let x = 0; x < w; x++) {
 
-				let xin = Math.floor((x * this.scale) % this.image.width);
-				let yin = Math.floor((y * this.scale) % this.image.height);
+				let xin = Math.floor((x * this.scale + this.warp(y)) % this.image.width);
+				let yin = Math.floor((y * this.scale + this.warp(x)) % this.image.height);
+
+				if (xin < 0) xin += this.image.width;
+				if (yin < 0) yin += this.image.height;
 
 				let p = this.game.canvas.getPixel(this.image, xin, yin);
 				im.putPixel(x + x0, y + y0, p);
