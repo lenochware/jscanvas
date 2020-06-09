5167431eb76fee5289f0327fbac1893314f16d65
lenochware <lenochware@gmail.com>
2020-06-09 11:18:30 +0200

    canvas texture

--- glcube.js
+++ glcube.js
@@ -10,6 +10,8 @@ class Main extends NextGameGL {
 		this.mesh = {}
 		this.light = null;
 
+		this.canvas2d = this.getCanvas2d();
+
 		this.initMesh();
 		this.createScene();
 		this.addLights();
@@ -26,6 +28,14 @@ class Main extends NextGameGL {
 		let material = new THREE.MeshLambertMaterial( { map: texture, color: '#FFFFFF' } );
 
 		this.mesh.cube = new THREE.Mesh( geometry, material );
+
+
+		const material2 = new THREE.MeshBasicMaterial({
+		  map: new THREE.CanvasTexture(this.canvas2d),
+		});
+
+		this.mesh.drawCube = new THREE.Mesh( geometry, material2 );
+
 	}
 
 	createScene()
@@ -35,8 +45,9 @@ class Main extends NextGameGL {
 		this.scene.add(m);
 		this.objects.push(m);
 
-		m = this.mesh.cube.clone();
+		m = this.mesh.drawCube.clone();
 		m.position.x += 1;
+		m.userData.updateTexture = true;
 		this.scene.add(m);
 		this.objects.push(m);
 
@@ -56,12 +67,37 @@ class Main extends NextGameGL {
 
 		const am = new THREE.AmbientLight( 0x404040 ); // soft white light
 		this.scene.add(am);
+	}
 
+
+	getCanvas2d()
+	{
+		const ctx = document.createElement('canvas').getContext('2d');
+		ctx.canvas.width = 256;
+		ctx.canvas.height = 256;
+		ctx.fillStyle = '#F00';
+		ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+		return ctx.canvas;	
+	}
+
+	drawCanvas()
+	{
+    let ctx = this.canvas2d.getContext('2d');
+    ctx.fillStyle = '#' + Utils.random(0, 0x1000000).toString(16).padStart(6, '0');
+    ctx.beginPath();
+
+    const x = Utils.random(0, 256);
+    const y = Utils.random(0, 256);
+    const radius = Utils.random(10, 64);
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.fill();
 	}
 
 	update()
 	{
 		this.requestUpdate();
+
+		this.drawCanvas();
 
 		if (this.kbmap['ArrowUp']) {
 			this.camera.position.z -= 0.1;
@@ -79,6 +115,8 @@ class Main extends NextGameGL {
 		}
 
 		for (let o of this.objects) {
+			if (o.userData.updateTexture) o.material.map.needsUpdate = true;
+
 			o.rotation.x += 0.01;
 			o.rotation.y += 0.01;
 		}
