4e6b629e4305ce3a89c9fcdd117156bcd50e70af
lenochware <lenochware@gmail.com>
2020-08-07 10:19:08 +0200

    merged iso.js

--- gldungeon.js
+++ gldungeon.js
@@ -1,5 +1,3 @@
-//Empty template
-
 class Main extends NextGameGL {
 
 	init()
@@ -12,21 +10,18 @@ class Main extends NextGameGL {
 		this.camera.lookAt(0, 0.5, 0);
 		this.camera.rotation.order = "YXZ"; 		
 
-		this.objects = {};
-		this.light = null;
-
-		const textureLoader = new THREE.TextureLoader();
-
-		let wall = {};
-
-		wall.texture = textureLoader.load( 'images/wallset_csb_front2.png' );
-		wall.texture.anisotropy = 16;
-		wall.geometry = new THREE.BoxGeometry(1,1,1);
-		wall.material = new THREE.MeshLambertMaterial( { map: wall.texture, color: '#FFFFFF' } );
-
-		this.objects.wall = wall;
+		this.level = this.level = JSON.parse(localStorage.getItem('lev01'));
 
 		this.player = new Player(this);
+
+		const textureLoader = new THREE.TextureLoader();
+		this.texture = textureLoader.load( 'images/dg_features32.gif' );
+		this.texture.magFilter = THREE.NearestFilter;
+		this.texture.minFilter = THREE.NearestFilter;
+		//this.texture.anisotropy = 16;
+
+		this.box = this.createBox();
+		this.material = new THREE.MeshLambertMaterial( { map: this.texture, color: '#FFFFFF' } );
 
 		this.createScene();
 		this.addLights();
@@ -34,29 +29,27 @@ class Main extends NextGameGL {
 
 	createScene()
 	{
-		let wall = this.objects.wall;
-		let m = new THREE.Mesh( wall.geometry, wall.material );
-		m.position.y = 0.5;
-		this.scene.add(m);
+		for(let key of Object.keys(this.level)) {
+			let pos = key.split(',');
+			let tex = this.level[key].tex;
+			let m = new THREE.Mesh(this.box[pos[2]].clone(), this.material);
+			m.position.set(pos[0] - 5, 0, pos[1] - 5);
+			m.name = key;
+			this.setTexture(m, tex);
+			this.scene.add(m);
+		}
+		this.addLights();
 	}
 
-	addWall(x, y, n)
-	{
-	}
 
 	addLights()
 	{
-		// create a point light
-		this.light = new THREE.PointLight(0xFFFFFF, 1);
+		// this.light = new THREE.PointLight(0xFFFFFF, 1);
+		// this.light.position.copy(this.camera.position);
+		// this.scene.add(this.light);		
 
-		// set its position
-		// this.light.position.x = 10;
-		// this.light.position.y = 50;
-		// this.light.position.z = 130;
-		this.light.position.copy(this.camera.position);
-
-		// add to the scene
-		this.scene.add(this.light);		
+		const am = new THREE.AmbientLight( 0xffffff ); // soft white light
+		this.scene.add(am);
 	}
 
 	update()
@@ -139,6 +132,81 @@ class Main extends NextGameGL {
 			pos.z += -Math.sin(this.camera.rotation.y) * .05;			
 		}
 	}
+
+	setTexture(m, idx)
+	{
+		 let w = 9;
+		 let h = 13;
+
+		 let u = idx % w / w;
+		 let v = Math.floor(idx / w) / h;
+
+		 let face = m.geometry.faceVertexUvs[0];
+		 face[0][0].set(u, v);
+		 face[0][1].set(u + 1/w, v + 1/h);
+		 face[0][2].set(u, v + 1/h);
+		 face[1][0].set(u, v);
+		 face[1][1].set(u + 1/w, v);
+		 face[1][2].set(u + 1/w, v + 1/h);
+
+		 m.geometry.uvsNeedUpdate = true;
+
+		 this.level[m.name] = {tex: idx};
+	}	
+
+	createPlane()
+	{
+		const geometry = new THREE.Geometry();
+		geometry.vertices.push(
+			new THREE.Vector3(-.5, -.5,  0),
+			new THREE.Vector3( .5, -.5,  0),
+			new THREE.Vector3(-.5,  .5,  0),
+			new THREE.Vector3( .5,  .5,  0)
+		);
+
+		geometry.faces.push(
+			new THREE.Face3(0, 3, 2),
+			new THREE.Face3(0, 1, 3)
+		);
+
+		geometry.faceVertexUvs[0].push(
+			[ new THREE.Vector2(0, 0), new THREE.Vector2(0, 0), new THREE.Vector2(0, 0) ],
+			[ new THREE.Vector2(0, 0), new THREE.Vector2(0, 0), new THREE.Vector2(0, 0) ],
+		);
+	 
+		return geometry;
+}
+
+	createBox()
+	{
+		let box = {};
+
+		box.px = this.createPlane();
+		box.px.rotateY( Math.PI / 2 );
+		box.px.translate( 0.5, 0, 0 );
+
+		box.nx = this.createPlane();
+		box.nx.rotateY( - Math.PI / 2 );
+		box.nx.translate( -0.5, 0, 0 );
+
+		box.py = this.createPlane();
+		box.py.rotateX( - Math.PI / 2 );
+		box.py.translate( 0, 0.5, 0 );
+
+		box.ny = this.createPlane();
+		// box.ny.rotateX( Math.PI / 2 );
+		box.ny.rotateX( - Math.PI / 2 );
+		box.ny.translate( 0, -0.5, 0 );
+
+		box.pz = this.createPlane();
+		box.pz.translate( 0, 0, 0.5 );
+
+		box.nz = this.createPlane();
+		box.nz.rotateY( Math.PI );
+		box.nz.translate( 0, 0, -0.5 );		
+
+		return box;
+	}	
 
 }
 
