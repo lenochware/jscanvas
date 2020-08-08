2265f4e6a6596c9bfe8ce6f1972762307b63aa06
lenochware <lenochware@gmail.com>
2020-08-08 11:01:09 +0200

    moving stuff to Player, +Player.moveAnimation

--- gldungeon.js
+++ gldungeon.js
@@ -3,17 +3,16 @@ class Main extends NextGameGL {
 	init()
 	{
 		super.init();
-		this.fullScreen();
+		//this.fullScreen();
 
-		// (left, up, backward)
-		this.camera.position.set(0, 0.2, 5);
-		this.camera.lookAt(0, 0.2, 0);
-		this.camera.rotation.order = "YXZ"; 		
+		this.camera.rotation.order = "YXZ";
+		//this.camera.position.set(x, this.height, y);
+		//this.camera.lookAt(0, 0.6, 0);
 
 		this.light = null;
 		this.level = this.level = JSON.parse(localStorage.getItem('lev01'));
 
-		this.player = new Player(this);
+		this.player = new Player(this, 0, 5);
 
 		const textureLoader = new THREE.TextureLoader();
 		this.texture = textureLoader.load( 'images/dg_features32.gif' );
@@ -63,82 +62,17 @@ class Main extends NextGameGL {
 	{
 		this.requestUpdate();
 
-		this.debugText(this.mouse.mx);
 		//this.debugText(this.camera.rotation);
 
+		//this.playerMove();
+		this.player.update();
 
-		if (!this.isPointerLock()) {
-			this.renderer.render( this.scene, this.camera );
-			return;
-		}
-
-		this.playerMove();
-
-		// if (this.kbmap['ArrowUp']) {
-		// 	this.camera.position.z -= 0.1;
-		// }
-
-		// if (this.kbmap['ArrowDown']) {
-		// 	this.camera.position.z += 0.1;
-		// }
-
-		// if (this.kbmap['ArrowLeft']) {
-		// 	this.camera.position.x -= 0.1;
-		// }
-
-		// if (this.kbmap['ArrowRight']) {
-		// 	this.camera.position.x += 0.1;
-		// }
-
-		// if (this.mouse.buttons) {
-		// 	this.mouse.buttons = 0;
-		// }
-
-		// this.camera.rotation.x = this.mouse.y / 400 - 1;
-		// this.camera.rotation.y = this.mouse.x / 800 - 1;
-
-		// this.light.position.set(
-		// 	this.camera.position.x,
-		// 	this.camera.position.y,
-		// 	this.camera.position.z
-		// )  
 
 		this.renderer.render( this.scene, this.camera );
 		this.light.position.copy(this.camera.position);
 
-		this.mouse.mx = 0;
-		this.mouse.my = 0;
-	}
-
-	playerMove()
-	{
-		let pos = this.camera.position;
-
-		this.camera.rotation.x -= (this.mouse.my / window.innerHeight) * Utils.TWO_PI;
-		this.camera.rotation.y -= (this.mouse.mx / window.innerWidth) * Utils.TWO_PI;
-
-		this.camera.rotation.x = Utils.clamp(this.camera.rotation.x, -1.5, 1.5);
-
-		if (this.kbmap['ArrowUp']) {
-			pos.x -= Math.sin(this.camera.rotation.y) * .05;
-			pos.z -= Math.cos(this.camera.rotation.y) * .05;	
-		}
-
-		if (this.kbmap['ArrowDown']) {
-			pos.x += Math.sin(this.camera.rotation.y) * .05;
-			pos.z += Math.cos(this.camera.rotation.y) * .05;
-		}
-
-
-		if (this.kbmap['ArrowLeft']) {
-			pos.x -= Math.cos(this.camera.rotation.y) * .05;
-			pos.z -= -Math.sin(this.camera.rotation.y) * .05;			
-		}
-
-		if (this.kbmap['ArrowRight']) {
-			pos.x += Math.cos(this.camera.rotation.y) * .05;
-			pos.z += -Math.sin(this.camera.rotation.y) * .05;			
-		}
+		this.kb.key = '';
+		this.mouse.mx = this.mouse.my = 0;
 	}
 
 	setTexture(m, idx)
@@ -193,27 +127,27 @@ class Main extends NextGameGL {
 
 		box.px = this.createPlane();
 		box.px.rotateY( Math.PI / 2 );
-		box.px.translate( 0.5, 0, 0 );
+		box.px.translate( 0.5, 0.5, 0 );
 
 		box.nx = this.createPlane();
 		box.nx.rotateY( - Math.PI / 2 );
-		box.nx.translate( -0.5, 0, 0 );
+		box.nx.translate( -0.5, 0.5, 0 );
 
 		box.py = this.createPlane();
 		box.py.rotateX( - Math.PI / 2 );
-		box.py.translate( 0, 0.5, 0 );
+		box.py.translate( 0, 1, 0 );
 
 		box.ny = this.createPlane();
 		// box.ny.rotateX( Math.PI / 2 );
 		box.ny.rotateX( - Math.PI / 2 );
-		box.ny.translate( 0, -0.5, 0 );
+		box.ny.translate( 0, 0, 0 );
 
 		box.pz = this.createPlane();
-		box.pz.translate( 0, 0, 0.5 );
+		box.pz.translate( 0, 0.5, 0.5 );
 
 		box.nz = this.createPlane();
 		box.nz.rotateY( Math.PI );
-		box.nz.translate( 0, 0, -0.5 );		
+		box.nz.translate( 0, 0.5, -0.5 );		
 
 		return box;
 	}	
@@ -222,13 +156,107 @@ class Main extends NextGameGL {
 
 class Player
 {
-	constructor(game)
+	constructor(game, x, y)
 	{
 		this.game = game;
-		this.camera = this.game.camera;
+		this.camera = game.camera;
+		this.height = 0.6;
+		this.setPos(x, y);
+		this.moveAnimation = null;	
+	}
 
-		this.x = 0;
-		this.y = 0;
-		this.rotateY = 0;
-	}	
+	startAnimation(id, duration)
+	{
+		this.moveAnimation = {id, duration, frame: 1};
+	}
+
+	playAnimation()
+	{
+		let anim = this.moveAnimation;
+		if (!anim) return;
+
+		if (anim.id == 'moveForward') this.moveForward();
+
+		anim.frame++;
+		if (anim.frame > anim.duration) {
+			this.moveAnimation = null;
+		}
+
+	}
+
+	moveForward()
+	{
+		console.log(this.moveAnimation.frame);
+	}
+
+	setPos(x, y)
+	{
+		this.pos = {x, y};
+		this.camera.position.set(x, this.height, y);
+	}
+
+	freeLook()
+	{
+		let rot = this.camera.rotation;
+		rot.x -= (this.game.mouse.my / window.innerHeight) * Utils.TWO_PI;
+		rot.y -= (this.game.mouse.mx / window.innerWidth) * Utils.TWO_PI;
+
+		rot.x = Utils.clamp(this.camera.rotation.x, -1.5, 1.5);		
+	}
+
+	listenKeyboard()
+	{
+		let key = this.game.kb.key;
+
+		if (key == 'ArrowUp') {
+			this.startAnimation('moveForward', 20);
+			console.log('start animation');
+		}
+	}
+
+	update()
+	{
+		this.listenKeyboard();
+		let anim = this.moveAnimation;
+		if (anim) this.playAnimation();
+	}
+
+	// playerMove()
+	// {
+	// 	let pos = this.camera.position;
+
+	// 	if (this.kbmap['ArrowUp']) {
+	// 		pos.x -= Math.sin(this.camera.rotation.y) * .05;
+	// 		pos.z -= Math.cos(this.camera.rotation.y) * .05;	
+	// 	}
+
+	// 	if (this.kbmap['ArrowDown']) {
+	// 		pos.x += Math.sin(this.camera.rotation.y) * .05;
+	// 		pos.z += Math.cos(this.camera.rotation.y) * .05;
+	// 	}
+
+
+	// 	if (this.kbmap['ArrowLeft']) {
+	// 		pos.x -= Math.cos(this.camera.rotation.y) * .05;
+	// 		pos.z -= -Math.sin(this.camera.rotation.y) * .05;			
+	// 	}
+
+	// 	if (this.kbmap['ArrowRight']) {
+	// 		pos.x += Math.cos(this.camera.rotation.y) * .05;
+	// 		pos.z += -Math.sin(this.camera.rotation.y) * .05;			
+	// 	}
+	// }
+
+	// moveForward(t)
+	// {
+	// 	let a = new THREE.Vector3(
+	// 		-Math.sin(this.camera.rotation.y), 
+	// 		0, 
+	// 		-Math.cos(this.camera.rotation.y)
+	// 	);
+
+	// 	let pos = this.camera.position;
+	// 	pos.x -= Math.sin(this.camera.rotation.y) * .05;
+	// 	pos.z -= Math.cos(this.camera.rotation.y) * .05;
+	// }
 }
